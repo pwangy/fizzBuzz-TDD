d4dae39aff824e11178bb45d79e766369be67bee
pwangy <blipsandclicks@gmail.com>
2024-02-04 23:27:07 -0800

    WIP

--- index.test.js
+++ index.test.js
@@ -5,6 +5,18 @@ describe('fizzBuzz', () => {
         expect(typeof fizzBuzz).toEqual('function')
     }),
     it('should return 1 for 1', () => {
-		expect(fizzBuzz(1)).toBe(1)
-	})
+        expect(fizzBuzz(1)).toEqual(1)
+    }), 
+    it('should return 2 for 2', () => {
+        expect(fizzBuzz(2)).toEqual(2)
+    }),
+    it('should return fizz when the number is divisible by 3', () => {
+        expect(fizzBuzz(3)).toEqual('Fizz')
+    })
 })
+
+// test('should return fizz when the number is divisible by 3', () => {
+//     expect(fizzBuzz(3)).toEqual('Fizz'),
+//     expect(fizzBuzz(6)).toBe('Fizz'),
+//     expect(fizzBuzz(12)).toBe('Fizz')
+// })

032655c0140380e0b885919f52c10d7d40e0a892
pwangy <blipsandclicks@gmail.com>
2024-02-04 22:47:05 -0800

    returns 1 for 1

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const fizzBuzz = () => {
-// something goes here
+    	for (let n = 1; n < 16; n++) {
+		// console.log(n)
+		if (n % 3 === 0) {
+			return 'Fizz'
+		} else {
+			return n
+		}
+	}
 }
 
 module.exports = fizzBuzz
--- index.test.js
+++ index.test.js
@@ -3,5 +3,8 @@ const fizzBuzz = require('./index')
 describe('fizzBuzz', () => {
     it('is a function', () => {
         expect(typeof fizzBuzz).toEqual('function')
-    })
+    }),
+    it('should return 1 for 1', () => {
+		expect(fizzBuzz(1)).toBe(1)
+	})
 })

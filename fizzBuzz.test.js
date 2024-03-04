8f179a93a1938282f1639a7b08e13f63d1bf3a91
pwangy <blipsandclicks@gmail.com>
2024-03-03 19:30:35 -0800

    show solution

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -3,13 +3,5 @@ const fizzBuzz = require('./fizzBuzz')
 describe('fizzBuzz', () => {
     it('is a function', () => {
         expect(typeof fizzBuzz).toEqual('function')
-    }),
-    it('should return the number if it is not divisible by 3 or 5', () => {
-        expect(fizzBuzz(1)).toBe(1)
-        expect(fizzBuzz(2)).toBe(2)
-        expect(fizzBuzz(4)).toBe(4)
-        expect(fizzBuzz(7)).toBe(7)
-        expect(fizzBuzz(8)).toBe(8)
-        expect(fizzBuzz(11)).toBe(11)
     })
 })

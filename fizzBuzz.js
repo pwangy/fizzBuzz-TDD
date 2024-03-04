270b6dab11f83ce84d61ec1b38f46f161d240037
pwangy <blipsandclicks@gmail.com>
2024-03-03 19:17:53 -0800

    reset to show start of 5th test

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,7 +1,5 @@
 const fizzBuzz = n => {
-	if (n % 3 === 0 && n % 5 === 0) {
-		return 'fizzbuzz'
-	} else if (n % 5 === 0) {
+	if (n % 5 === 0) {
 		return 'buzz'
 	} else if (n % 3 === 0) {
 		return 'fizz'
